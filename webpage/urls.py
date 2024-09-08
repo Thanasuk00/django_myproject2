40fcb0cf1e4c6ab823a92dae0f1b3e6be8c0d5d6
Thanasuk00 <thanasuk4745@gmil.com>
2024-09-08 14:23:33 +0700

    commit

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path("",  views.indexPage, name="index"),
     path("about/", views.aboutUs, name="about"),
-    path("contact/", views.contactUs),
+    path("contact/", views.contactUs, name="contact"),
 ]
